f712deab5be71177854a162dfddac4b217c64d6c
Grigore Budac <contact@grigorebudac.com>
2022-04-18 11:43:45 +0200

    insert the user into db right after confirmation

--- apps/infrastructure/triggers/postConfirmation/index.ts
+++ apps/infrastructure/triggers/postConfirmation/index.ts
@@ -1,10 +1,27 @@
 import { PostConfirmationTriggerHandler } from "aws-lambda";
+import AWS from "aws-sdk";
+
+const DynamoDB = new AWS.DynamoDB();
 
 export const handler: PostConfirmationTriggerHandler = async (
   event,
   context,
   callback
 ) => {
-  console.log("--> add user to dynamodb");
+  const { sub, email } = event.request.userAttributes;
+  const currentUnixTime = Date.now().toString();
+
+  const params = {
+    TableName: process.env.USERS_TABLE!,
+    Item: {
+      id: { S: sub },
+      email: { S: email },
+      createdAt: { S: currentUnixTime },
+      updatedAt: { S: currentUnixTime },
+    },
+  };
+
+  await DynamoDB.putItem(params).promise();
+
   callback(null, event);
 };
